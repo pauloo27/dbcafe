21e16b04b795c1db3e07d835278c792cc58e1ae0
Paulo <git@pauloo27.dev>
2022-05-29 16:04:57 -0300

    feat(api): call the api and handle effects

--- components/inputs/text.js
+++ components/inputs/text.js
@@ -1,15 +1,32 @@
 import styles from '@styles/inputs.module.css'
+import { forwardRef } from 'react'
 
-export default function TextInput({
-  placeholder, label, type, helper,
-}) {
+export function textInput({
+  placeholder, label, type, helper, onChange,
+  onEnter,
+}, ref) {
+  const onKeyDown = (e) => {
+    if (e.key !== 'Enter') return
+    e.preventDefault()
+    onEnter()
+  }
   return (
     <label className={styles.textInputLabel}>
       <div className={styles.textInputLabelHelperContainer}>
         {label}
         {helper}
       </div>
-      <input className={styles.textInput} type={type ?? 'text'} placeholder={placeholder} />
+      <input
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        ref={ref}
+        className={styles.textInput}
+        type={type ?? 'text'}
+        placeholder={placeholder}
+      />
     </label>
   )
 }
+const TextInput = forwardRef(textInput)
+
+export default TextInput
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import styles from '@styles/home.module.css'
 import TextInput from '@components/inputs/text'
 import Button from '@components/inputs/button'
 import Checkbox from '@components/inputs/checkbox'
+import { useEffect, useState, useRef } from 'react'
 
 function forgotPassword() {
   return (
@@ -9,7 +10,37 @@ function forgotPassword() {
   )
 }
 
+const LoginStatus = {
+  NULL: 'NULL',
+  PENDING: 'PENDING',
+  SUCCESS: 'SUCCESS',
+  FAILURE: 'FAILURE',
+}
+
 export default function Home() {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
+  const [status, setStatus] = useState(LoginStatus.NULL)
+
+  useEffect(() => {
+    if (status === LoginStatus.SUCCESS) {
+      window.location.href = 'https://mail.db.cafe/webmail'
+    }
+  }, [status]);
+
+  const handleSubmit = () => {
+    setStatus(LoginStatus.PENDING)
+    const url = 'https://mail.db.cafe/sso/login'
+    const data = new FormData()
+    data.append('email', emailRef.current.value)
+    data.append('pw', passwordRef.current.value)
+    data.append('submitWebmail', 'Sign in Webmail')
+    fetch(url, { method: 'POST', body: data }).then((res) => {
+      setStatus(res.status === 200 && res.redirected ? LoginStatus.SUCCESS : LoginStatus.FAILURE)
+    })
+  }
+
   return (
     <div className={styles.mainContainer}>
       <div className={styles.leftContainer}>
@@ -22,12 +53,25 @@ export default function Home() {
         <div className={styles.titleContainer}>
           <h1 className={styles.title}>DB Cafe</h1>
           <h2 className={styles.subtitle}>The best coffee in cyber space</h2>
+          {
+            status === LoginStatus.FAILURE &&
+            <h3 className={styles.status}>Incorrect email or password</h3>
+          }
         </div>
         <div className={styles.loginForm}>
-          <TextInput label="E-mail" placeholder="name@example.com" />
-          <TextInput label="Password" helper={forgotPassword()} type="password" placeholder="********" />
+          <TextInput
+            ref={emailRef} label="E-mail" placeholder="name@example.com"
+          />
+          <TextInput
+            ref={passwordRef}
+            label="Password"
+            onEnter={handleSubmit}
+            helper={forgotPassword()}
+            type="password"
+            placeholder="********"
+          />
           <Checkbox label="Remember-me" />
-          <Button className={styles.loginButton} label="Login" />
+          <Button onClick={handleSubmit} className={styles.loginButton} label="Login" />
           <span>
             Not a member?
             {' '}
