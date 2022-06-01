75e019aca029b59c0f1e2fefe1d0069b9af681e8
Paulo <git@pauloo27.dev>
2022-06-01 09:14:20 -0300

    fix: move meta tag to app
    since next asked me to

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,6 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
         <link href="https://fonts.googleapis.com/css2?family=Kadwa&display=swap" rel="stylesheet" />
       </Head>
       <body>
