7084602f8cc7005385557b26a6df10cf3329981c
Paulo <git@pauloo27.dev>
2022-06-05 02:36:57 -0300

    feat: add title to login page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import styles from '@styles/home.module.css'
 import TextInput from '@components/inputs/text'
 import Button from '@components/inputs/button'
@@ -42,45 +43,50 @@ export default function Home() {
   }
 
   return (
-    <div className={styles.mainContainer}>
-      <div className={styles.leftContainer}>
-        <a href="#">Contact us</a>
-        <a target="_blank" href="https://unsplash.com/photos/eQu4HfK53lk" rel="noreferrer">
-          Photo by Naveen Prajapat on Unsplash
-        </a>
-      </div>
-      <div className={styles.rightContainer}>
-        <div className={styles.titleContainer}>
-          <h1 className={styles.title}>DB Cafe</h1>
-          <h2 className={styles.subtitle}>The best coffee in cyber space</h2>
-          {
+    <>
+      <Head>
+        <title>DB Cafe - Login</title>
+      </Head>
+      <div className={styles.mainContainer}>
+        <div className={styles.leftContainer}>
+          <a href="#">Contact us</a>
+          <a target="_blank" href="https://unsplash.com/photos/eQu4HfK53lk" rel="noreferrer">
+            Photo by Naveen Prajapat on Unsplash
+          </a>
+        </div>
+        <div className={styles.rightContainer}>
+          <div className={styles.titleContainer}>
+            <h1 className={styles.title}>DB Cafe</h1>
+            <h2 className={styles.subtitle}>The best coffee in cyber space</h2>
+            {
             status === LoginStatus.FAILURE
             && <h3 className={styles.status}>Incorrect email or password</h3>
           }
-        </div>
-        <div className={styles.loginForm}>
-          <TextInput
-            ref={emailRef}
-            label="E-mail"
-            placeholder="name@example.com"
-          />
-          <TextInput
-            ref={passwordRef}
-            label="Password"
-            onEnter={handleSubmit}
-            helper={forgotPassword()}
-            type="password"
-            placeholder="********"
-          />
-          <Checkbox label="Remember-me" />
-          <Button onClick={handleSubmit} className={styles.loginButton} label="Login" />
-          <span>
-            Not a member?
-            {' '}
-            <a href="#">Ask for an account</a>
-          </span>
+          </div>
+          <div className={styles.loginForm}>
+            <TextInput
+              ref={emailRef}
+              label="E-mail"
+              placeholder="name@example.com"
+            />
+            <TextInput
+              ref={passwordRef}
+              label="Password"
+              onEnter={handleSubmit}
+              helper={forgotPassword()}
+              type="password"
+              placeholder="********"
+            />
+            <Checkbox label="Remember-me" />
+            <Button onClick={handleSubmit} className={styles.loginButton} label="Login" />
+            <span>
+              Not a member?
+              {' '}
+              <a href="#">Ask for an account</a>
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
